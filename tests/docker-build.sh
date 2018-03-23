c8e566e33e7233fae71e34c492eea26e2f76c86f
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-03-23 12:05:18 +0200

    tests: Fix build errors in docker environment
    At the moment building of umockdev is failing but we do not need it
    since all needed changes were incorporated upstream and packaged at
    least for debian/testing. This saves us time building umockdev in the
    container out of sources.

--- tests/docker-build.sh
+++ tests/docker-build.sh
@@ -5,15 +5,7 @@ set -e
 # Print executed commands
 set -x
 
-# Building in /usr/local/src
-pushd ..
-# Required commits are: bd14fb6 and 88d2cc3
-git clone https://github.com/martinpitt/umockdev.git
-pushd umockdev
-./autogen.sh --prefix=/usr && make -j4 && make install && popd
-popd
-
-echo "Returning to " `pwd`
+echo "Building in " `pwd`
 
 rm -rf build && mkdir build
 cd build && cmake .. && cmake --build .
