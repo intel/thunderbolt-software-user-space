7e90055cd8ebd3afd535d5c7b835b260cff21d54
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-04-06 14:20:10 +0300

    tests: Add testing tools built with standard libs
    Add testing when using std::filesystem

--- tests/docker-build.sh
+++ tests/docker-build.sh
@@ -7,7 +7,18 @@ set -x
 
 echo "Building in " `pwd`
 
+# Build for boost libraries
 rm -rf build && mkdir build
-cd build && cmake .. && cmake --build .
+pushd build && cmake .. && cmake --build .
 
 LC_ALL=C.UTF-8 make check
+
+popd
+
+# Build for standard libraries
+rm -rf build && mkdir build
+pushd build && cmake .. -Duse-std-filesystem=ON && cmake --build .
+
+LC_ALL=C.UTF-8 make check
+
+popd
