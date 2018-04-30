a9267e1172fa5142ea12b4a4b562d5b42be9aa00
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-04-30 12:42:46 +0300

    tests: Rename test-integration-mock to test_integration_mock
    Rename since python have problems sourcing file otherwise.

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ l = (dir(thunderbolt_test))
 tests = [ x for x in l if x.startswith('test')]
 
 for test in tests:
-    cmdline = "umockdev-wrapper python3 tests/test-integration-mock.py thunderbolt_test.%s" % test
+    cmdline = "umockdev-wrapper python3 tests/test_integration_mock.py thunderbolt_test.%s" % test
     result = subprocess.run(shlex.split(cmdline))
 
     if result.returncode:
